f61c6257970f2a0364644490cd934af85bae9d1c
harshada-05-code <wankhedeharshada14@gmail.com>
2026-03-30 22:06:03 +0530

    fixing error

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,8 @@ import App from './App.jsx'
 import './index.css' 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+
+
 
 
 
