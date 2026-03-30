0e8e02cea62f86028ca4d61d478106aac2dff8e8
harshada-05-code <wankhedeharshada14@gmail.com>
2026-03-30 23:11:57 +0530

    Fix: update tailwindcss v4 import syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,9 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  darkMode: 'class',
   theme: {
-    extend: {
-      colors: {
-        primary: "#6366f1",
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
+
